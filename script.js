a416d7f60f89876456bffc11e508cc37f0afe671
LanaMama <101367331+LanaMama@users.noreply.github.com>
2026-05-01 16:19:25 +0200

    edit Swiper JS

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 // import Swiper JS
-import Swiper from 'swiper';
+// import Swiper from 'swiper';
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal', // Горизонтально
+    loop: true,
+    slidesPerView: 3, // Сколько картинок видно сразу
+    spaceBetween: 20, // Расстояние между ними
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
